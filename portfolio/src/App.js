471cc61e6a0ecc7706e32143c93b5f42c94a0cb1
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-20 17:50:01 -0500

    updated app to include header footer and nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
-import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <Nav/>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
