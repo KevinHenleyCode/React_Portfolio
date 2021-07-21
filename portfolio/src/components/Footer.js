4b515655ab566a47186957ec08e8a49150450cae
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-20 23:16:14 -0500

    added styling

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
     return(
-        <div>
-        <ul>
+        <div className='bg-gray-500 mt-auto my-20 py-10'>
+        <ul className='flex justify-around'>
             <li>GitHub</li>
             <li>LinkedIn</li>
             <li>StackOverflow</li>
--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Nav() {
     return(
-        <div>
-        <ul>
+        <div className='bg-gray-500 mr-auto mb-20 py-10'>
+        <ul className='flex justify-around'>
             <li>About Me</li>
             <li>Portfolio</li>
             <li>Contact</li>
