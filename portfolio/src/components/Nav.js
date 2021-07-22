744f90d93e4b7655bf36a32481d157850593c64d
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-22 15:50:27 -0500

    added the page functionality

--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Nav() {
+function Nav({ currentPage, pageChange }) {
     return(
-        <div className='bg-gray-500 mr-auto mb-20 py-4'>
-        <ul className='flex justify-around'>
-            <li>About Me</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-            <li>Resume</li>
+        <div className='bg-gray-500 mr-auto py-2 sm:py-4'>
+        <ul className='flex flex-col items-center sm:flex-row sm:justify-around'>
+            <li><a href="#AboutMe" onClick={() => pageChange ('AboutMe')} className={currentPage === 'aboutMe' ? 'underline text-blue-400' : 'text-black hover:text-blue-300'}>About Me</a></li>
+            <li><a href="#Portfolio" onClick={() => pageChange ('Portfolio')} className={currentPage === 'portfolio' ? 'underline text-blue-400' : 'text-black'}>Portfolio</a></li>
+            <li><a href="#Contact" onClick={() => pageChange ('Contact')} className={currentPage === 'contact' ? 'underline text-blue-400' : 'text-black'}>Contact</a></li>
+            <li><a href="#Resume" onClick={() => pageChange ('Resume')} className={currentPage === 'resume' ? 'underline text-blue-400' : 'text-black'}>Resume</a></li>
         </ul>
         </div>
     )
