999d22fd2b25573f05dd5271b31f3fd558688479
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-23 00:14:00 -0500

    rmoved the container component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,12 @@
 import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Container from "./components/Container"
 
 
 function App() {
   return (
     <div className=' bg-gray-800 flex flex-col h-screen'>
       <Header/>
-      <Container className='flex-grow'/>
       <Footer/>
     </div>
   );
