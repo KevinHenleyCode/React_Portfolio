3e9dd9652553dd065a2e2defe8825ce0df60ebcd
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-08-25 14:17:53 -0500

    added email link

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -9,7 +9,7 @@ function Contact() {
                     <h3>For more questions you can reach me at my GitHub or Email: </h3>
                    <ul className='flex flex-col mt-10'>
                        <li className='mb-5 list-none no-underline text-red-900 hover:text-white'><a href="https://github.com/KevinHenleyCode">GitHub</a></li>
-                       <li className='list-none no-underline text-red-900 hover:text-white'><a href="kevinhenleyinfo@gmail.com">kevinhenleyinfo@gmail.com</a></li>
+                       <li className='list-none no-underline text-red-900 hover:text-white'><a href="mailto: kevinhenleyinfo@gmail.com">kevinhenleyinfo@gmail.com</a></li>
                    </ul>
                 </div>
             </div>
