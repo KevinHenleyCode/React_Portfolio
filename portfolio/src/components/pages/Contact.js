3d5b09d36ca5d4946e796c91f26ecb0e7854faa8
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-08-25 13:47:43 -0500

    fixed h2 error

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
     return(
          <div className='flex flex-col justify-center m-auto h-auto mt-auto mt-20 py-4 px-10 sm:py-10 w-11/12 lg:w-1/2'>
             <div className='mb-20 pb-10 shadow-sm bg-gray-400'>
-                    <h2 className='flex justify-center bg-black text-white text-xl sm:text-5xl py-2'>Contact</h2>
+                <h2 className='flex justify-center bg-black text-white text-xl sm:text-5xl py-2'>Contact</h2>
                 <div className='px-4 my-5'>
                     <h3>For more questions you can reach me at my GitHub or Email: </h3>
                    <ul className='flex flex-col mt-10'>
