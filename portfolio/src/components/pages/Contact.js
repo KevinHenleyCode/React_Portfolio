d0493a71e9f1437df276cf241eda770022b7e9af
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-09-15 15:38:20 -0500

    added word break class to the email

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -2,14 +2,19 @@ import React from 'react'
 
 function Contact() {
     return(
-         <div className='flex flex-col justify-center m-auto h-auto mt-auto mt-20 py-4 px-10 sm:py-10 w-11/12 lg:w-1/2'>
+         <div className='flex flex-col justify-center break-words m-auto h-auto mt-auto mt-20 py-4 px-10 sm:py-10 w-11/12 lg:w-1/2'>
             <div className='mb-20 pb-10 shadow-sm rounded-xl bg-gray-400'>
                 <h2 className='flex justify-center bg-black rounded-t-xl text-white text-xl sm:text-5xl py-2'>Contact</h2>
                 <div className='px-4 my-5'>
                     <h3>For more questions you can reach me at my GitHub or Email: </h3>
-                   <ul className='flex flex-col mt-10'>
-                       <li className='mb-5 list-none no-underline text-red-900 hover:text-white'><a href="https://github.com/KevinHenleyCode">GitHub</a></li>
-                       <li className='list-none no-underline text-red-900 hover:text-white'><a href="mailto:kevinhenleyinfo@gmail.com">kevinhenleyinfo@gmail.com</a></li>
+                    <div className='flex flex-col mt-10'>
+                        <a className='mb-5 list-none no-underline text-red-900 hover:text-white' href="https://github.com/KevinHenleyCode">GitHub</a>
+                        <br/>
+                        <a className='list-none no-underline text-red-900 hover:text-white' href="mailto:kevinhenleyinfo@gmail.com">kevinhenleyinfo@gmail.com</a>
+                    </div>
+                   <ul >
+                       <li ></li>
+                       <li ></li>
                    </ul>
                 </div>
             </div>
