f9897e2df994039043f65a0a484aae93076975c5
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-09-19 16:59:36 -0500

    updated stackoverflow icon style

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
         <ul className='flex flex-col items-center sm:flex-row sm:justify-around'>
             <li className='mb-4'><a href="https://github.com/KevinHenleyCode"><img className='w-6 sm:w-10' src={GitHub} alt="img" /></a></li>
             <li className='mb-4'><a href="https://www.linkedin.com/in/kevin-henley/"><img className='w-6 sm:w-10' src={LinkedIn} alt="img" /></a></li>
-            <li className=''><a href="https://stackoverflow.com/users/16102427/kevinhenleycode?tab=profile"><img className='w-6 sm:w-10' src={StackOverflow} alt="img" /></a></li>
+            <li className='mb-4'><a href="https://stackoverflow.com/users/16102427/kevinhenleycode?tab=profile"><img className='w-6 sm:w-10' src={StackOverflow} alt="img" /></a></li>
         </ul>
         </footer>
     )
