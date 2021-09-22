e702a2e24a8e6b7f5c9bc1e5d9798de401251385
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-09-21 22:48:31 -0500

    changed buttons from yellow to green

--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Nav({ currentPage, pageChange }) {
     return(
-            <div className='flex flex-col sm:items-end sm:bg-transparent bg-yellow-500 items-center mr-auto py-2 sm:py-4'>
+            <div className='flex flex-col sm:items-end sm:bg-transparent bg-green-500 items-center mr-auto py-2 sm:py-4'>
                 <ul className='flex flex-col items-center sm:flex-row sm:justify-around'>
-                    <li className='sm:mr-20 sm:bg-yellow-500 p-1 sm:rounded-md sm:shadow-sm'><a href="#AboutMe" onClick={() => pageChange ('AboutMe')} className={currentPage === 'AboutMe' ? 'text-white' : 'text-black hover:text-white font-bold'}>About Me</a></li>
-                    <li className='sm:mr-20 sm:bg-yellow-500 p-1 sm:rounded-md sm:shadow-sm'><a href="#Portfolio" onClick={() => pageChange ('Portfolio')} className={currentPage === 'Portfolio' ? 'text-white' : 'text-black hover:text-white font-bold'}>Portfolio</a></li>
-                    <li className='sm:mr-20 sm:bg-yellow-500 p-1 sm:rounded-md sm:shadow-sm'><a href="#Contact" onClick={() => pageChange ('Contact')} className={currentPage === 'Contact' ? 'text-white' : 'text-black hover:text-white font-bold'}>Contact</a></li>
-                    <li className='sm:mr-20 sm:bg-yellow-500 p-1 sm:rounded-md sm:shadow-sm'><a href="#Resume" onClick={() => pageChange ('Resume')} className={currentPage === 'Resume' ? 'text-white' : 'text-black hover:text-white font-bold'}>Resume</a></li>
+                    <li className='sm:mr-20 sm:bg-green-500 p-1 sm:rounded-md sm:shadow-sm'><a href="#AboutMe" onClick={() => pageChange ('AboutMe')} className={currentPage === 'AboutMe' ? 'text-white' : 'text-black hover:text-white font-bold'}>About Me</a></li>
+                    <li className='sm:mr-20 sm:bg-green-500 p-1 sm:rounded-md sm:shadow-sm'><a href="#Portfolio" onClick={() => pageChange ('Portfolio')} className={currentPage === 'Portfolio' ? 'text-white' : 'text-black hover:text-white font-bold'}>Portfolio</a></li>
+                    <li className='sm:mr-20 sm:bg-green-500 p-1 sm:rounded-md sm:shadow-sm'><a href="#Contact" onClick={() => pageChange ('Contact')} className={currentPage === 'Contact' ? 'text-white' : 'text-black hover:text-white font-bold'}>Contact</a></li>
+                    <li className='sm:mr-20 sm:bg-green-500 p-1 sm:rounded-md sm:shadow-sm'><a href="#Resume" onClick={() => pageChange ('Resume')} className={currentPage === 'Resume' ? 'text-white' : 'text-black hover:text-white font-bold'}>Resume</a></li>
                 </ul>
         </div>
     )
