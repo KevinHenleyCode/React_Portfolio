0160527d9080a29cce3b6da466bab60765a35ae8
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-09-26 22:34:04 -0500

    made header sticky

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -5,8 +5,8 @@ import Container from './Container'
 function Header() {
     return(
         <div>
-            <header className='flex justify-center py-3 text-white text-6xl bg-black h-20 shadow-2xl'>Portfolio</header>
-             <Container/>
+            <header className='flex justify-center py-3 text-white text-6xl bg-black h-20 shadow-2xl sticky top-0 z-50'>Portfolio</header>
+             <Container className='relative'/>
         </div>
     )
 }
